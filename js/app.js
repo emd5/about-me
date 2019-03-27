d88e842cd8bc9cfbb779e7e2b220ef04d6fe6ed7
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-03-27 08:59:09 -0700

    update eslint file and cleaned up code with linter

--- js/app.js
+++ js/app.js
@@ -19,55 +19,56 @@ var correctAnswerBank = ['Yes', 'Yes', 'Yes', 'No', 'Yes' ];
 var userInput = [];
 for (var i=0; i<questionBank.length; i++){
   userInput[i] = prompt(questionBank[i]);
-    if (userInput[i].toLowerCase() === 'yes'){
-      console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
-    } else if( userInput[i].toLowerCase() === 'no') {
-      console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
-    }  
-    console.log('The correct answer is: ' + correctAnswerBank[i]);
-    alert(userName + '! The correct answer is ' + correctAnswerBank[i]);
+  if (userInput[i].toLowerCase() === 'yes'){
+    console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
+  } else if( userInput[i].toLowerCase() === 'no') {
+    console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
+  }
+  console.log('The correct answer is: ' + correctAnswerBank[i]);
+  alert(userName + '! The correct answer is ' + correctAnswerBank[i]);
 }
 
 //Calculate score
 var score = 0;
-for (var i=0; i<userInput.length; i++){
+// eslint-disable-next-line no-redeclare
+for (var i = 0; i<userInput.length; i++){
   if (userInput[i].toLowerCase() === correctAnswerBank[i].toLowerCase()){
     score++;
-  } 
+  }
 }
-var result = (score/5)*100
+var result = (score/5)*100;
 alert(userName+ ', you know ' + result + '% of me!');
 
 //Game message
 switch(result){
-  case 20:
-    alert('You don\'t even know me');
-    break;
-  case 40:
-    alert('Boo! You can do better to know me');
-    break;
-  case 60:
-    alert('Meh, we need to hang out more');
-    break;
-  case 80:
-    alert('Getting there');
-    break;
-  case 100:
-    alert('Let\'s Code more!');
-    break;
-  default:
-    alert('Somehow you broke my game!!!!');
+case 20:
+  alert('You don\'t even know me');
+  break;
+case 40:
+  alert('Boo! You can do better to know me');
+  break;
+case 60:
+  alert('Meh, we need to hang out more');
+  break;
+case 80:
+  alert('Getting there');
+  break;
+case 100:
+  alert('Let\'s Code more!');
+  break;
+default:
+  alert('Somehow you broke my game!!!!');
 }
 
 //Displays username on HTML page
-document.getElementById("username").innerHTML = userName;
+document.getElementById('username').innerHTML = userName;
 
 //Function which displays the Q/A 
+// eslint-disable-next-line no-unused-vars
 function displayResults(){
   var text = '';
   for(var i=0; i<questionBank.length; i++){
     text += questionBank[i] + '<br> Your response: ' + userInput[i] + ' | Correct Answer: ' + correctAnswerBank[i] + '<br>';
   }
   document.getElementById("results").innerHTML = text;
-
 }
