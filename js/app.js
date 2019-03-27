b12cd5a5f7b4fe00e672ee743de5cf6db0979de1
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-03-26 21:20:11 -0700

    changed html layout, applied css

--- js/app.js
+++ js/app.js
@@ -13,33 +13,33 @@ if(userName.toLowerCase() === 'liz'){
 var questionBank = ['Do I like coding? ','Do I love front end development? ','Do I love working in back end development? ','Do I like bugs?', 'Do I love to write in python?'];
 
 var correctAnswerBank = ['Yes', 'Yes', 'Yes', 'No', 'Yes' ];
-
 var userInput = [];
 
 for (var i=0; i<questionBank.length; i++){
   userInput[i] = prompt(questionBank[i]);
     if (userInput[i].toLowerCase() === 'yes'){
       console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
-      console.log('The correct answer is: ' + correctAnswerBank[i]);
+      console.log(userName +'The correct answer is: ' + correctAnswerBank[i]);
 
     } else if( userInput[i].toLowerCase() === 'no') {
       console.log('Your response for question ' + (i+1) +' is: ' + userInput[i]);
       console.log('The correct answer is: ' + correctAnswerBank[i]);
 
     }  
-    alert('The correct answer is ' + correctAnswerBank[i]);
+    alert(userName + '! The correct answer is ' + correctAnswerBank[i]);
 }
 
+//Calculate score
 var score = 0;
 for (var i=0; i<userInput.length; i++){
   if (userInput[i].toLowerCase() === correctAnswerBank[i].toLowerCase()){
     score++;
   } 
 }
-
 var result = (score/5)*100
 alert('Your score is: ' + result + '%');
 
+//Game message
 switch(result){
   case 20:
     alert('You don\'t know me');
@@ -57,12 +57,26 @@ switch(result){
     alert('Let\'s Code more!');
     break;
   default:
-    alert('Somehow you broke my code!!!!');
+    alert('Somehow you broke my game!!!!');
+}
+
+//Displays username on HTML page
+document.getElementById("username").innerHTML = userName;
+
+//Function which displays the Q/A 
+function displayResults(){
+  var text = '';
+  for(var i=0; i<questionBank.length; i++){
+    text += questionBank[i] + ' Your response: ' + userInput[i] + ' | Correct Answer: ' + correctAnswerBank[i] + '<br>';
+  }
+  document.getElementById("results").innerHTML = text;
+
 }
 
 
 
-document.getElementById("username").innerHTML = userName;
+
+
 
 
 
